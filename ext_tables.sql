caadc96c532d50032e8254d0a4f24143b938d78c
René Alessandri <rene@alessandri-media.ch>
2021-08-16 10:36:34 +0200

    [BUGFIX] Add card header and footer fields to ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,5 +3,7 @@
 #
 CREATE TABLE tt_content (
     tx_contentelements_card_flip_enable smallint(5) unsigned DEFAULT '0' NOT NULL,
+    tx_contentelements_card_header varchar(255) DEFAULT '' NOT NULL,
+    tx_contentelements_card_footer varchar(255) DEFAULT '' NOT NULL,
     tx_contentelements_bodytext text
 );

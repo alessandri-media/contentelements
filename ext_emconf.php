a451612c9ce41c147e62e5f914c42cd78a4502d3
René Alessandri <rene@alessandri-media.ch>
2021-08-30 12:44:22 +0200

    [TASK] Raise version to 0.0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,5 +29,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'René Alessandri',
     'author_email' => 'rene@alessandri-media.ch',
     'author_company' => 'Alessandri Media',
-    'version' => '0.0.3',
+    'version' => '0.0.4',
 ];

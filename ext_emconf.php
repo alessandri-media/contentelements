3136ae453ddedc0d0bbfb76bdb2d12e785a10e7b
René Alessandri <rene@alessandri-media.ch>
2021-11-02 09:29:03 +0100

    [BUGFIX] Use new version number for required extensions

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '10.4.0-10.4.99',
             'fluid_styled_content' => '10.4.0-10.4.99',
             'rte_ckeditor' => '10.4.0-10.4.99',
-            'devlib' => '0.0.0-0.99.99'
+            'devlib' => '10.4.0-10.4.99'
         ],
         'conflicts' => [
         ],

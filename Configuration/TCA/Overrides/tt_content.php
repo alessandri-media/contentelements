d4f118f782b9afe9a632602b3b0639f54d446f47
René Alessandri <rene@alessandri-media.ch>
2021-12-13 14:18:21 +0100

    [TASK] Add rendering options for containers and card

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,9 +1,9 @@
 <?php
 
 $additionalColumns = [
-    'tx_contentelements_card_header' => [
+    'tx_contentelements_card_header'          => [
         'exclude'   => 1,
-        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:tx_contentelements_card_header',
+        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_card_header',
         'l10n_mode' => 'prefixLangTitle',
         'config'    => [
             'type' => 'input',
@@ -11,9 +11,9 @@ $additionalColumns = [
             'max'  => 255,
         ],
     ],
-    'tx_contentelements_card_footer' => [
+    'tx_contentelements_card_footer'          => [
         'exclude'   => 1,
-        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:tx_contentelements_card_footer',
+        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_card_footer',
         'l10n_mode' => 'prefixLangTitle',
         'config'    => [
             'type' => 'input',
@@ -21,48 +21,112 @@ $additionalColumns = [
             'max'  => 255,
         ],
     ],
-    'tx_contentelements_card_flip_enable' => [
-        'exclude' => 1,
-        'label'   => 'LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:tx_contentelements_card_flip_enable',
+    'tx_contentelements_card_flip_enable'     => [
+        'exclude'  => 1,
+        'label'    => 'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_card_flip_enable',
         'onChange' => 'reload',
-        'config'  => [
-            'type'    => 'check',
+        'config'   => [
+            'type'       => 'check',
             'renderType' => 'checkboxToggle',
-            'items' => [
+            'items'      => [
                 [
-                    0                    => '',
-                    1                    => 1
+                    0 => '',
+                    1 => 1,
                 ],
             ],
         ],
     ],
-    'tx_contentelements_bodytext'         => [
-        'exclude' => 1,
-        'label'   => 'LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:tx_contentelements_bodytext',
+    'tx_contentelements_card_image_overlay'   => [
+        'exclude'   => 1,
+        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_card_image_overlay',
         'l10n_mode' => 'prefixLangTitle',
-        'config'  => $GLOBALS['TCA']['tt_content']['columns']['bodytext']['config'],
+        'config'    => [
+            'type' => 'check',
+        ],
+    ],
+    'tx_contentelements_animation'            => [
+        'exclude'   => 1,
+        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_animation',
+        'l10n_mode' => 'prefixLangTitle',
+        'config'    => [
+            'type'       => 'select',
+            'renderType' => 'selectSingle',
+            'items'      => [
+                [
+                    'None',
+                    '',
+                ],
+            ],
+        ],
+    ],
+    'tx_contentelements_background_color'     => [
+        'exclude'   => 1,
+        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_background_color',
+        'l10n_mode' => 'prefixLangTitle',
+        'config'    => [
+            'type'       => 'select',
+            'renderType' => 'selectSingle',
+            'items'      => [
+                [
+                    'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_background_color_none',
+                    '',
+                ],
+            ],
+        ],
+    ],
+    'tx_contentelements_container_rendermode' => [
+        'exclude'   => 1,
+        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_container_rendermode',
+        'l10n_mode' => 'prefixLangTitle',
+        'config'    => [
+            'type'       => 'select',
+            'renderType' => 'selectSingle',
+            'items'      => [
+                [
+                    'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_container_rendermode_none',
+                    '',
+                ],
+                [
+                    'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_container_rendermode_boxed',
+                    'container',
+                ],
+                [
+                    'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_container_rendermode_fluid',
+                    'container-fluid',
+                ],
+            ],
+        ],
+    ],
+    'tx_contentelements_container_no_gutters' => [
+        'exclude'   => 1,
+        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_container_no_gutters',
+        'l10n_mode' => 'prefixLangTitle',
+        'config'    => [
+            'type' => 'check',
+        ],
+    ],
+    'tx_contentelements_bodytext'             => [
+        'exclude'   => 1,
+        'label'     => 'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_bodytext',
+        'l10n_mode' => 'prefixLangTitle',
+        'config'    => $GLOBALS['TCA']['tt_content']['columns']['bodytext']['config'],
     ],
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $additionalColumns);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'tt_content',
-    'CType',
-    [
-        'LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:content.element.card',
-        'contentelements_card',
-        'content-text',
-    ],
-    'textmedia',
-    'after'
+    'tx_contentelements_container_rendermode, tx_contentelements_background_color, tx_contentelements_container_no_gutters',
+    'container_grid_25_25_25_25,container_grid_25_75,container_grid_33_33_33,container_grid_33_66,container_grid_50_50,container_grid_66_33,container_grid_75_25,container_grid_100',
+    'after:header'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
     'CType',
     [
-        'LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:content.element.carousel',
+        'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:content.element.carousel',
         'contentelements_carousel',
         'content-text',
     ],
@@ -74,7 +138,7 @@ $additionalColumns = [
     'tt_content',
     'CType',
     [
-        'LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:content.element.responsive_image',
+        'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:content.element.responsive_image',
         'contentelements_responsive_image',
         'content-text',
     ],
@@ -86,7 +150,7 @@ $additionalColumns = [
     'tt_content',
     'CType',
     [
-        'LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:content.element.logo_wall',
+        'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:content.element.logo_wall',
         'contentelements_logo_wall',
         'content-text',
     ],
@@ -98,7 +162,7 @@ $additionalColumns = [
     'tt_content',
     'CType',
     [
-        'LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:content.element.accordion_item',
+        'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:content.element.accordion_item',
         'contentelements_accordion_item',
         'content-text',
     ],
@@ -115,8 +179,8 @@ $GLOBALS['TCA']['tt_content']['types']['contentelements_card'] = [
             tx_contentelements_card_header,
             image;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:image_formlabel,
             bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
-            tx_contentelements_card_flip_enable;LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:tx_contentelements_card_flip_enable,
-            tx_contentelements_bodytext;LLL:EXT:contentelements/Resources/Private/Language/locallang.xlf:tx_contentelements_bodytext_card_back,
+            tx_contentelements_card_flip_enable;LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_card_flip_enable,
+            tx_contentelements_bodytext;LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:tx_contentelements_bodytext_card_back,
             tx_contentelements_card_footer,
             header_link;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_link,
          --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
@@ -124,20 +188,20 @@ $GLOBALS['TCA']['tt_content']['types']['contentelements_card'] = [
             --palette--;;access,       
     ',
     'columnsOverrides' => [
-        'image'    => [
+        'image'                       => [
             'config' => [
                 'maxitems' => 1,
             ],
         ],
-        'bodytext' => [
+        'bodytext'                    => [
             'config' => [
-                'enableRichtext'        => true
+                'enableRichtext' => true,
             ],
         ],
         'tx_contentelements_bodytext' => [
             'displayCond' => 'FIELD:tx_contentelements_card_flip_enable:=:1',
-            'config' => [
-                'enableRichtext'        => true
+            'config'      => [
+                'enableRichtext' => true,
             ],
         ],
     ],
@@ -187,7 +251,7 @@ $GLOBALS['TCA']['tt_content']['types']['contentelements_logo_wall'] = [
 ];
 
 $GLOBALS['TCA']['tt_content']['types']['contentelements_accordion_item'] = [
-    'showitem' => '
+    'showitem'         => '
          --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
             --palette--;;general,
             header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_formlabel,
@@ -199,10 +263,36 @@ $GLOBALS['TCA']['tt_content']['types']['contentelements_accordion_item'] = [
     'columnsOverrides' => [
         'bodytext' => [
             'config' => [
-                'enableRichtext'        => true
+                'enableRichtext' => true,
             ],
         ],
     ],
 ];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    'tx_contentelements_background_color, tx_contentelements_animation',
+    'contentelements_card',
+    'after:CType'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    'tx_contentelements_card_image_overlay',
+    'contentelements_card',
+    'after:image'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'LLL:EXT:contentelements/Resources/Private/Language/locallang_db.xlf:content.element.card',
+        'contentelements_card',
+        'content-text',
+    ],
+    'textmedia',
+    'after'
+);
 
 
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,8 +2,13 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
-    tx_contentelements_card_flip_enable smallint(5) unsigned DEFAULT '0' NOT NULL,
+    tx_contentelements_background_color varchar(255) DEFAULT '' NOT NULL,
+    tx_contentelements_animation varchar(255) DEFAULT '' NOT NULL,
     tx_contentelements_card_header varchar(255) DEFAULT '' NOT NULL,
     tx_contentelements_card_footer varchar(255) DEFAULT '' NOT NULL,
+    tx_contentelements_card_flip_enable smallint(5) unsigned DEFAULT '0' NOT NULL,
+    tx_contentelements_card_image_overlay smallint(5) unsigned DEFAULT '0' NOT NULL,
+    tx_contentelements_container_rendermode varchar(255) DEFAULT '' NOT NULL,
+    tx_contentelements_container_no_gutters smallint(5) unsigned DEFAULT '0' NOT NULL,
     tx_contentelements_bodytext text
 );
